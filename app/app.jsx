632c0ad4da2666cd8f989056a70cf302e6c53d41
Glitch (100-oscillators) <none>
2018-11-28 13:17:40 +0000

    🏉🎄 Updated with Glitch

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+/* Import Components */
+const Synth = require('./components/Synth');
+
+ReactDOM.render(<Synth/>, document.getElementById('main'));
